20e5952ced8a39a7728cfaaa098cad7f0b3b306f
Bgoutami27 <goutamu9438721075@gmail.com>
2025-10-26 22:40:40 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ 
-  baseURL: `${process.env.REACT_APP_API_URL}/api`
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL + '/api', // make sure no trailing slash in .env
 });
 
 export default API;
