d66473ae091ec8f0b85e33c583d6cf46c0744720
Bgoutami27 <goutamu9438721075@gmail.com>
2025-10-26 19:58:45 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://project-management-tool-tuns.onrender.com' });
 
 export default API;
