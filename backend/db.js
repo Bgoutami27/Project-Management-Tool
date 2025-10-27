ea053493a9c28b578089f36f7e0b24fcfe910da1
Bgoutami27 <goutamu9438721075@gmail.com>
2025-10-27 12:38:15 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,15 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false, // important for Render
+  },
 });
 
 pool.on('connect', () => {
-    console.log('Connected to PostgreSQL');
+  console.log('✅ Connected to PostgreSQL');
 });
 
 module.exports = pool;
